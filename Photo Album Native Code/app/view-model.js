a2fb2efc8cb2d8236d435e9cc8c923bbeb61f1b9
Anton Dobrev <anton.dobrev@telerik.com>
2015-03-13 10:17:29 +0000

    Completed app with Telerik Backend Services integration.

--- Photo Album Native Code/app/view-model.js
+++ Photo Album Native Code/app/view-model.js
@@ -2,6 +2,17 @@ var observable = require("data/observable");
 var observableArrayModule = require("data/observable-array");
 var imageSourceModule = require("image-source");
 var fileSystemModule = require("file-system");
+var Everlive = require('./everlive.all.min');
+
+
+var bsApiKey = 'YOUR_API_KEY';
+var bsUrlScheme = 'https';
+
+var everlive = new Everlive({
+    apiKey: bsApiKey,
+    scheme: bsUrlScheme
+});
+
 
 var directory = "/res/";
 
@@ -61,9 +72,23 @@ var PhotoAlbumModel = (function (_super) {
     return PhotoAlbumModel;
 })(observable.Observable);
 
+var backendArray = new observableArrayModule.ObservableArray();
 Object.defineProperty(PhotoAlbumModel.prototype, "photoItems", {
     get: function () {
-        return array;
+        everlive.Files.get().then(function (data) {
+                data.result.forEach(function (fileMetadata) {
+                    var fileUrl = fileMetadata.Uri;
+                    imageSourceModule.fromUrl(fileUrl).then(function (imageFromUrl) {
+                        var item = {
+                            itemImage: imageFromUrl
+                        };
+                        backendArray.push(item);
+                    });
+                });
+            },
+            function (error) {});
+
+        return backendArray;
     },
     enumerable: true,
     configurable: true
@@ -74,6 +99,19 @@ PhotoAlbumModel.prototype.tapAction = function () {
     array.push(item8);
 
     this.set("message", "Images added. Total images: " + array.length);
+
+    // convert the local files to base64 string and upload them in the Telerik Backend Services project
+    for (i = 0; i < array.length; i++) {
+        var file = {
+            "Filename": Math.random().toString(36).substring(2, 15) + ".jpg",
+            "ContentType": "image/jpeg",
+            "base64": array.getItem(i).itemImage.toBase64String("JPEG", 100)
+        };
+
+        everlive.Files.create(file,
+            function (data) {},
+            function (error) {});
+    }
 };
 
 exports.PhotoAlbumModel = PhotoAlbumModel;
